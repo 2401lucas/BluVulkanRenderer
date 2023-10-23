61591732b66dcb5920ff38655d4fca3438fdc566
Abbey Perry <thecarguru2401@gmail.com>
2023-10-23 10:42:39 -0300

    save v0.2

---
+++ BluRendererVulkan/BluRendererVulkan/src/Render/Descriptors/DescriptorUtils.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <vulkan/vulkan_core.h>
+#include <vector>
+
+class DescriptorUtils {
+public:
+	DescriptorUtils() = delete;
+
+	static VkDescriptorSetLayoutBinding createDescriptorSetBinding(const uint32_t& binding, const uint32_t& descriptorCount, const VkDescriptorType& descriptorType, const VkSampler& immutableSamplers, const VkShaderStageFlags& shaderStageFlags);
+};
