5e619db379784ba7ec7dd589b0df499f0257e1ea
Lucas McCully <thecarguru2401@gmail.com>
2024-01-16 22:33:12 -0400

    [Refactor] Cleaned Files

--- BluRendererVulkan/BluRendererVulkan/src/Engine/Model/Model.cpp
+++ BluRendererVulkan/BluRendererVulkan/src/Engine/Model/Model.cpp
@@ -5,14 +5,5 @@ Model::Model(const SceneModel& modelInfo) {
 }
 
 void Model::cleanup() {
-    for (auto component : components) {
-        //component->cleanup();
-        delete component;
-    }
-}
 
-//The ID of components is derived from the order of the components given when creating an Entity
-BaseComponent* Model::getComponent(uint32_t componentID)
-{
-	return components.at(componentID);
 }
--- BluRendererVulkan/BluRendererVulkan/src/Engine/Model/Model.h
+++ BluRendererVulkan/BluRendererVulkan/src/Engine/Model/Model.h
@@ -3,17 +3,12 @@
 #include <memory>
 #include "../Mesh/Mesh.h"
 #include "../src/Engine/Scene/SceneUtils.h"
-#include "Components/BaseComponent.h"
 
 class Model {
 public:
 	Model(const SceneModel& modelPath);
     void cleanup();
 
-	BaseComponent* getComponent(uint32_t componentID);
-
-	uint32_t componentID;
 private:
-	//TODO: Ensure memory is contiguous 
-	std::vector<BaseComponent*> components;
+	
 };
