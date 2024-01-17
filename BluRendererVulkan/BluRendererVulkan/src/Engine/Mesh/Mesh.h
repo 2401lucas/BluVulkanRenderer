2bcff2b5b50c54ba2dfe9f1d2ccadbd0ce88f91a
Lucas McCully <thecarguru2401@gmail.com>
2024-01-16 22:33:12 -0400

    [Refactor] Cleaned Files

--- BluRendererVulkan/BluRendererVulkan/src/Engine/Mesh/Mesh.h
+++ BluRendererVulkan/BluRendererVulkan/src/Engine/Mesh/Mesh.h
@@ -3,54 +3,7 @@
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/glm.hpp>
 #include <glm/gtx/hash.hpp>
-#include <vulkan/vulkan_core.h>
-
-
-struct Vertex {
-    glm::vec3 pos;
-    glm::vec3 color;
-    glm::vec3 normal;
-    glm::vec2 texCoord;
-
-    static VkVertexInputBindingDescription getBindingDescription() {
-        VkVertexInputBindingDescription bindingDescription{};
-        bindingDescription.binding = 0;
-        bindingDescription.stride = sizeof(Vertex);
-        bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
-
-        return bindingDescription;
-    }
-
-    static std::array<VkVertexInputAttributeDescription, 4> getAttributeDescriptions() {
-        std::array<VkVertexInputAttributeDescription, 4> attributeDescriptions{};
-
-        attributeDescriptions[0].binding = 0;
-        attributeDescriptions[0].location = 0;
-        attributeDescriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
-        attributeDescriptions[0].offset = offsetof(Vertex, pos);
-
-        attributeDescriptions[1].binding = 0;
-        attributeDescriptions[1].location = 1;
-        attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
-        attributeDescriptions[1].offset = offsetof(Vertex, color);
-
-        attributeDescriptions[2].binding = 0;
-        attributeDescriptions[2].location = 2;
-        attributeDescriptions[2].format = VK_FORMAT_R32G32B32_SFLOAT;
-        attributeDescriptions[2].offset = offsetof(Vertex, normal);
-
-        attributeDescriptions[3].binding = 0;
-        attributeDescriptions[3].location = 3;
-        attributeDescriptions[3].format = VK_FORMAT_R32G32_SFLOAT;
-        attributeDescriptions[3].offset = offsetof(Vertex, texCoord);
-
-        return attributeDescriptions;
-    }
-
-    bool operator==(const Vertex& other) const {
-        return pos == other.pos && color == other.color && texCoord == other.texCoord && normal == other.normal;
-    }
-};
+#include "../Model/Components/MeshRendererComponent.h"
 
 namespace std {
     template<> struct hash<Vertex> {
---
+++ BluRendererVulkan/BluRendererVulkan/src/Engine/Model/Components/InputComponent.h
@@ -0,0 +1,9 @@
+#pragma once
+
+struct Input {
+public:
+	float mouseX;
+	float mouseY;
+	float horizontal;
+	float vertical;
+};
